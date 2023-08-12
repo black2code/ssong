3b197e7368a74d0e14fc0b8b8d7893587cb45352
black2code <alsdnr1080@gmail.com>
2023-08-12 19:56:38 +0900

    Feat : Entity 설계

---
+++ src/main/java/com/example/ssong/food/entity/Food.java
@@ -0,0 +1,10 @@
+package com.example.ssong.food.entity;
+
+
+public class Food {
+    private String name;
+    private String description;
+    private Long price;
+    private String cookingTime;
+    private String type;  // 메인, 사이드, 음료
+}
